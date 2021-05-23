a35ffe042e49510ba712243ffc5445268bfa3333
bitbybytes <61658325+blythebit18@users.noreply.github.com>
2021-05-23 21:54:01 +0800

    Update 2wd Arduino Car.ino

--- 2wd Arduino Car.ino
+++ 2wd Arduino Car.ino
@@ -7,119 +7,140 @@
 
 NewPing sonar(Trig_Pin, Echo_Pin, Max_Distance);
 
-int distance;
+float distance;
 
 Servo SG90;
-const int servoCenter = 100;
-const int angle = 45;
-const int servoPin = 9;
 
-const int in1 = 1;
-const int in2 = 2;
+#define in1 11
+#define in2 2
 
-const int in3 = 3;
-const int in4 = 4;
+#define in3 3
+#define in4 4
 
 const int enA = 5;
 const int enB = 6;
 
-int maxSpeed = 200;
+#define right 20
+#define left 110
+#define center 65
+
+
 
 void setup() {
+Serial.begin(9600);
+SG90.attach(9);
 pinMode(enA, OUTPUT);
 pinMode(enB, OUTPUT);
 pinMode(in1, OUTPUT);
 pinMode(in2, OUTPUT);
 pinMode(in3, OUTPUT);
 pinMode(in4, OUTPUT);
-SG90.attach(servoPin);
-SG90.write(servoCenter);
-
+SG90.write(center);
 }
 
 void loop() {
-if(Distance()<10){
-  Stop();
-  delay(500);
-    if(lookRight()<10 and lookLeft()<10){
-      Reverse();
-      delay(700);
-      }else if(lookRight() > lookLeft()){
-        Right();
-        delay(300);
-        }else{
-          Left();
-          delay(300);
-          }
-}else{
-  Straight();
+
+if(Distance()<25){
+ stop();
+ delay(1000);
+  if(lookRight()<25 and lookLeft()<25){
+    delay(800);
+    moveBackward();
+    delay(300);
+    stop();
+    }else if(lookLeft() > lookRight()){
+     delay(500);
+     moveLeft();
+     delay(700);
+     stop();
+        
+  }else{
+    delay(500);
+    moveRight();
+    delay(700);
+    stop();
   }
+}else moveForward();
 
 }
 
 int Distance(){
 distance = sonar.ping_cm();
+Serial.println(distance);
+delay(300);
 return distance;
+
+
 }
 
-int lookRight(){
-SG90.write(servoCenter - angle);
-delay(500);
-distance = Distance();
-SG90.write(servoCenter);
-delay(600);
-return distance;
-}
-
-int lookLeft(){
-SG90.write(servoCenter + angle);
-delay(500);
-distance = Distance();
-SG90.write(servoCenter);
-delay(600);
-return distance;  
-}
-
-
-void Straight(){
-analogWrite(enA, maxSpeed);
-analogWrite(enB, maxSpeed);
-digitalWrite(in1, LOW);
-digitalWrite(in2, HIGH);
-digitalWrite(in3, LOW);
-digitalWrite(in4, HIGH); 
-}
-
-void Reverse(){
-analogWrite(enA, 100);
-analogWrite(enB, 100);
+void moveForward(){
+analogWrite(enA, 90);
+analogWrite(enB, 90);
 digitalWrite(in1, HIGH);
 digitalWrite(in2, LOW);
 digitalWrite(in3, HIGH);
-digitalWrite(in4, LOW); 
+digitalWrite(in4, LOW);
 }
 
-void Stop(){
-digitalWrite(in1, LOW);
-digitalWrite(in2, LOW);
-digitalWrite(in3, LOW);
-digitalWrite(in4, LOW);  
-}
-
-void Left(){
+void moveBackward(){
 analogWrite(enA, 100);
 analogWrite(enB, 100);
 digitalWrite(in1, LOW);
 digitalWrite(in2, HIGH);
 digitalWrite(in3, LOW);
-digitalWrite(in4, LOW);  
+digitalWrite(in4, HIGH);
+Serial.println("Move Backward");
 }
 
-void Right(){
-analogWrite(enA, 100);
+void moveRight(){
+analogWrite(enA, 0);
 analogWrite(enB, 100);
 digitalWrite(in1, LOW);
 digitalWrite(in2, LOW);
+digitalWrite(in3, HIGH);
+digitalWrite(in4, LOW);
+Serial.println("Turn Right");
+}
+
+void moveLeft(){
+analogWrite(enA, 100);
+analogWrite(enB, 0);
+digitalWrite(in1, HIGH);
+digitalWrite(in2, LOW);
 digitalWrite(in3, LOW);
-digitalWrite(in4, HIGH);  
+digitalWrite(in4, LOW);
+Serial.println("Turn Left");
+}
+
+void stop(){
+analogWrite(enA, 0);
+analogWrite(enB, 0);
+digitalWrite(in1, LOW);
+digitalWrite(in2, LOW);
+digitalWrite(in3, LOW);
+digitalWrite(in4, LOW);
+Serial.println("Stop");
+}
+
+int lookRight(){
+SG90.write(right);
+Serial.println("look Right");
+Serial.print("Right Distance: ");
+delay(500);
+int distance = Distance();
+SG90.write(center);
+delay(300);
+return distance;
+
+
+}
+
+int lookLeft(){
+SG90.write(left);
+Serial.print("Left Distance: ");
+delay(500);
+int distance = Distance();
+SG90.write(center);
+delay(300);
+return distance;
 }
